df2af00397a377767bd95870a383bda9292f9a87
fyr91 <feng0049@gmail.com>
2023-02-23 10:25:29 +0800

    added action config

---
+++ action_config.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# @Author: fyr91
+# @Date:   2023-02-22 16:30:27
+# @Last Modified by:   fyr91
+# @Last Modified time: 2023-02-22 19:06:05
+fall_desc = [
+	"falling", 
+	"falling on the ground",
+	"falling on the floor",
+	"lying on the ground",
+	"lying on the floor",
+	"tripping over"
+]
+
+fight_desc = [
+	"fighting",
+	"slam a person",
+	"striking a person",
+	"hititing a person",
+	"hitting each other",
+	"kick each other",
+	"punching each other",
+	# "slapping each other"
+	# "wrestling"
+]
+
+nudity_desc = [
+	"topless",
+	"naked",
+	"exposing breasts",
+	"exposing anus",
+	"exposing genitalia",
+	"exposing breasts",
+	"exposing breasts",
+	"exposing belly",
+	"exposing nipples"
+]
+
+fight_doc_desc = [
+	"fighting a doctor",
+	# "strike a doctor",
+	# "kick a doctor",
+	"punching a doctor",
+	"slam a doctor",
+	# "slapping a doctor",
+	"hitting a doctor",
+	# "strike a nurse",
+	"fighting a nurse",
+	# "kick a nurse",
+	"punching a nurse",
+	"slam a nurse",
+	# "slapping a nurse",
+	"hitting a nurse",
+	# "fighting a epidemic prevention personnel",
+	# "kick a epidemic prevention personnel",
+	# "punching a epidemic prevention personnel",
+	# "slapping a epidemic prevention personnel",
+	# "hitting a epidemic prevention personnel",
+]
+
+suicide_desc = [
+	"commiting suicide by hanging",
+	"hanging and strugglling"
+	# "commiting suicide by hanging around neck"
+
+]
+
+climb_desc = [
+	"climbing over",
+	"climbing",
+]
+
+gather_desc = [
+	"people gathering",
+	"cluster of people",
+	"people clustering",
+	"people procession"
+]
+
+violence_desc = [
+	"destroying public facilities",
+	"kick stuff",
+	# "kick door",
+	# "kick wall",
+	"punching stuff",
+	# "punching door",
+	# "punching wall",
+	"hitting stuff",
+	# "hitting door",
+	# "hitting wall",
+	"riot behaviour",
+	"violent behaviour"
+]
+
+exclusion_desc = [
+	"sweeping floor",
+	"cleaning floor",
+	"pushing up"
+]
--- clip_server.py
+++ clip_server.py
@@ -2,7 +2,7 @@
 # @Author: fyr91
 # @Date:   2023-02-13 15:40:14
 # @Last Modified by:   fyr91
-# @Last Modified time: 2023-02-16 17:41:47
+# @Last Modified time: 2023-02-22 19:02:31
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
@@ -13,6 +13,8 @@ import os
 import clip
 import torch
 import torch.nn as nn
+
+from action_config import *
 
 from datasets import ActionFrames
 from torch.utils.data import DataLoader
@@ -40,79 +42,38 @@ class ImageCLIP(nn.Module):
 
 
 # preset configs
-num_seg = 1
+num_seg = 8 # frames to process per seg
 sim_header = "Transf" #Transf   meanP  LSTM  Conv_1D  Transf_cls
 vit_bone = "weights/ViT-B-16.pt"
 pretrain = "weights/vit-b-16-16f.pt"
-batch_size = 8
+batch_size = 1
 data_workers = 1
 input_size = 224
-label_data = [
-	"falling", # fall 0
-	"fighting", # fight 1
-	"exposed breasts", # nudity 2
-	"exposed anus", # nudity 3
-	"exposed genitalia", # nudity 4
-	"topless", # nudity 5
-	"fighting with a doctor", # fight doctor 6
-	"fighting with a nurse", # fight doctor 7
-	"suicide by hanging on a rope", # suicide 8
-	"climbing over window", # climb over 9
-	"climbing through window", # climb over 10
-	"climbing over wall", # climb over 11
-	"gathering", # gathering 12
-	"destroy public facilities", # violence 13
-	"kicking stuff", # violence 14
-	"kick door", # violence 15
-	"traffic jam", # exclusion 16
-	"driving", # exclusion 17
-	"climbing stairs", # exclusion 18
-	"climbing over fence", # climb over 19
-	"fall on the ground", # fall 20
-	"argue with a doctor" # exclusion 21
-	"suicide by hanging around neck", # suicide 22
-	"topless man", # nudity 23
-	"walking", # exclusion 24
-	"pointing each other", # exlusion 25
-	"walking pass each other", # exclusion 26
-	"hitting walls", # violence 27
-	"sweeping floor", # exclusion 28
-	"washing floor", # exclusion 29
-	"cleaning", # exclusion 30
-	"drinking", # exclusion 31
-	"falling on the ground", # falling 32
-	"slippery", # slippery 33
-	"lying on the floor", # falling 34
-	"lying on the ground", # falling 35
-	"kicking a doctor" # falling 36
-	"punching a doctor" # falling 37
-	"exposing belly", # nudity 38
-	"exposing back", # nudity 39
-	"exposed nipple", # nudity 40
-	"clustering", # gatehring 41
+label_data = [*fall_desc,*fight_desc,*nudity_desc,
+			  *fight_doc_desc,*suicide_desc,*climb_desc,
+			  *gather_desc,*violence_desc,*exclusion_desc]
 
-]
-
-fall_idx = [0,20,32,33,34,35]
-fight_idx = [1]
-nutidy_idx = [2,3,4,5,23,38,39,40]
-fight_doc_idx = [6,7,36,37]
-suicide_idx = [8, 22]
-climb_idx = [9,10,11,19]
-gathering_idx = [12, 41]
-violence_idx = [13,14,15,27]
-exclusion_idx = [16, 17, 18, 21, 24, 25, 26, 28, 29, 30, 31]
+# target actions
+fall_idx = [label_data.index(a) for a in fall_desc]
+fight_idx = [label_data.index(a) for a in fight_desc]
+nutidy_idx = [label_data.index(a) for a in nudity_desc]
+fight_doc_idx = [label_data.index(a) for a in fight_doc_desc]
+suicide_idx = [label_data.index(a) for a in suicide_desc]
+climb_idx = [label_data.index(a) for a in climb_desc]
+gathering_idx = [label_data.index(a) for a in gather_desc]
+violence_idx = [label_data.index(a) for a in violence_desc]
+excluded_idx = [label_data.index(a) for a in exclusion_desc]
 
 # in seq fall, fight, nude, fight-doc, suicide, climb, gather, violence
 threshs = {
-	"falling":1.1, 
-	"fighting":0.71, 
-	"nudity":1.1,
-	"fighting_doc":1.1, 
+	"falling":0.17, 
+	"fighting":0.65, 
+	"nudity":0.9,
+	"fighting_doc":0.5, 
 	"suicide":0.76, 
-	"climbing":1.1, 
+	"climbing":0.9, 
 	"gathering":0.71, 
-	"violence":1.1
+	"violence":0.6
 	}
 
 
@@ -183,7 +144,7 @@ async def cls(segs: List[str] = Form()):
 	data = ActionFrames([segs], data_transform)
 	loader = DataLoader(data, batch_size=batch_size, num_workers=data_workers, 
 		shuffle=False, pin_memory=True, drop_last=False)
-	print("created data loader")
+	print("INFO:     created data loader")
 
 	res = {}
 	event_idx = []
@@ -199,6 +160,9 @@ async def cls(segs: List[str] = Form()):
 		"gathering":-1, 
 		"violence":-1
 	}
+	action_details = {}
+	for label in label_data:
+		action_details[label] = -1
 
 	with torch.no_grad():
 		for ii, image in enumerate(loader):
@@ -217,10 +181,14 @@ async def cls(segs: List[str] = Form()):
 			values_5, indices_5 = similarity.topk(5, dim=-1)
 
 			t1 = time.time()
+			print("INFO:     =================================")
 			for i in range(b):
 				res[i] = []
 				for j in range(5):
 					temp = {}
+					if action_details[label_data[indices_5[i][j]]] < values_5[i][j].item():
+						action_details[label_data[indices_5[i][j]]] = values_5[i][j].item()
+
 					if indices_5[i][j] in fall_idx:
 						if highest["falling"] < values_5[i][j].item():
 							highest["falling"] = values_5[i][j].item()
@@ -270,9 +238,16 @@ async def cls(segs: List[str] = Form()):
 							temp = {"event":"violence", "score":values_5[i][j].item()}
 							res[i].append(temp)
 
+		for detail_key in action_details:
+			if action_details[detail_key] != -1:
+				print(f"INFO:     actions - {detail_key} - {round(action_details[detail_key], 4)}")
+
 		# print(f"video seg features generated in {t1-t0}")
-		for item in highest.items():
-			print(item)
+		print("INFO:     =================================")
+		for item_key in highest:
+			if highest[item_key] != -1:
+				print(f"INFO:     {item_key}: {round(highest[item_key], 4)}")
+		print("INFO:     =================================")
 	# check what event:
-	print(res)
+	print(f"INFO:     res - {res}")
 	return res
